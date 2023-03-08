d07cb3d7f2271de4bb405f71d98f92e54bb4e77d
ThandiweZhanje <107370221+ThandiweZhanje@users.noreply.github.com>
2023-03-08 13:20:45 +0200

    3.1

--- main (3).c
+++ main (3).c
@@ -9,9 +9,21 @@
 #include "lcd_stm32f0.h"
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
-
+struct age_data my_age;
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
+typedef struct
+{
+	  uint8_t day;
+	  uint8_t month;
+	  uint16_t year;
+	  uint8_t age;
+
+}age_data; // initialises a struct containing age data
+
+
+
+
 
 void main(void);                                                   //COMPULSORY
 
